18e71f6c19f6a13b54bcb3a36f5a011906e4671d
Alfredo Mendoza <92193407+4lfr3d0MP@users.noreply.github.com>
2022-11-22 23:52:59 -0600

    Add files via upload
    se agregaron los archivos del programa

---
+++ Generador_de_señales/Generador_de_funciones_completo/Form1.Designer.cs
@@ -0,0 +1,235 @@
+﻿namespace Generador_de_funciones_completo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben eliminar; false en caso contrario, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.grafica1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.trackBar2 = new System.Windows.Forms.TrackBar();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.grafica1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // grafica1
+            // 
+            this.grafica1.BackColor = System.Drawing.Color.Ivory;
+            this.grafica1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.grafica1.Location = new System.Drawing.Point(385, 12);
+            this.grafica1.Name = "grafica1";
+            this.grafica1.Size = new System.Drawing.Size(539, 368);
+            this.grafica1.TabIndex = 0;
+            this.grafica1.TabStop = false;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.button1.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(221, 258);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(64, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Iniciar";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(303, 258);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(74, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Reinicia";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick_1);
+            // 
+            // listBox1
+            // 
+            this.listBox1.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.listBox1.Font = new System.Drawing.Font("MS Reference Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Items.AddRange(new object[] {
+            "1.- Seno cuadrado",
+            "2.- Seno",
+            "",
+            "3.- Media onda",
+            "4.- Triangular",
+            "",
+            "5.- Diente sierra positivo",
+            "6.- Tangente",
+            "",
+            "7.- Coseno",
+            "8.- Seno positivo",
+            "",
+            "9.- Diente inverso alterno",
+            "10.- Diente inverso positivo"});
+            this.listBox1.Location = new System.Drawing.Point(15, 53);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(184, 228);
+            this.listBox1.TabIndex = 3;
+            this.toolTip1.SetToolTip(this.listBox1, "Cambiar de pulso");
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.BackColor = System.Drawing.Color.Black;
+            this.trackBar1.Location = new System.Drawing.Point(221, 176);
+            this.trackBar1.Maximum = 15;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(146, 45);
+            this.trackBar1.SmallChange = 2;
+            this.trackBar1.TabIndex = 8;
+            this.trackBar1.Value = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(217, 153);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(98, 20);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Frecuencia";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // trackBar2
+            // 
+            this.trackBar2.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.trackBar2.Location = new System.Drawing.Point(221, 83);
+            this.trackBar2.Maximum = 5;
+            this.trackBar2.Name = "trackBar2";
+            this.trackBar2.Size = new System.Drawing.Size(146, 45);
+            this.trackBar2.TabIndex = 10;
+            this.trackBar2.Value = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(217, 60);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(79, 20);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Amplitud";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(28, 32);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(149, 18);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "Señales Electricas";
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.button3.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(15, 345);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(64, 23);
+            this.button3.TabIndex = 14;
+            this.button3.Text = "Firma";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(159)))), ((int)(((byte)(53)))), ((int)(((byte)(93)))));
+            this.ClientSize = new System.Drawing.Size(934, 391);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.trackBar2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.grafica1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.ShowInTaskbar = false;
+            this.Text = "Generador de Señales Electricas";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.grafica1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox grafica1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.ListBox listBox1;
+        //private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TrackBar trackBar2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ Generador_de_señales/Generador_de_funciones_completo/Form1.cs
@@ -0,0 +1,277 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Generador_de_funciones_completo
+{
+    public partial class Form1 : Form
+    { 
+        private double x, y, xi, xf, yi, yf, h,t1,T1,A1,A2;
+
+        private void lbFirma_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("WPF_Mendoza.exe");
+        }
+
+        private void Amplitud_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            System.Diagnostics.Process.Start("WPF_Mendoza.exe");
+        }
+
+        private int c, f, ci, cf, fi, ff, n, c1, f1, habilitar;
+        
+        public Form1()
+        {
+            habilitar = 1;
+            t1 = 0; 
+            InitializeComponent();
+            A1 = 1;
+
+        }
+        
+        public void Graficador(Graphics g1)
+        {
+            ci = 1;
+            cf = grafica1.Width;
+            fi = 1;
+            ff = grafica1.Height;
+            n = cf - ci;
+            h = (xf - xi) / n;
+           
+            
+            yi =-10;
+            yf = 10;
+            
+            if (((yi) * (yf)) < 0)
+            {
+                y = 0;
+                f = Fil();
+                g1.DrawLine(Pens.LightGray, ci, f, cf, f);
+            }
+            if (((xi) * (xf)) < 0)
+            {
+                x = 0;
+                c = Col();
+                g1.DrawLine(Pens.LightGray, c, fi, c, ff);
+            }
+
+            string sx;
+            Font drawFont = new Font("Arrow", 7);
+            SolidBrush drawBrush = new SolidBrush(Color.Black);
+            int n2 = (int)xf;
+
+            x = xi;
+            
+            string xs;
+            string ys;
+          
+            for (int k2 = (int)xi; k2 <= n2; k2++)
+            {
+                y = 0;
+                c = Col();
+                f = Fil();
+                xs = x.ToString();
+
+                sx = xs;
+                g1.DrawString(sx, drawFont, drawBrush, c, f);
+                g1.DrawLine(Pens.LightGray, c, fi, c, ff);
+               
+                x = x + 1;
+            }
+
+            int n3 = (int)yf;
+
+            y = (int)yi;
+
+            for (int k3 = (int)yi; k3 <= n3; k3++)
+            {
+                x = 0;
+                c = Col();
+                f = Fil();
+                ys = y.ToString();
+
+                sx = ys;
+                g1.DrawString(sx, drawFont, drawBrush, c, f);
+                g1.DrawLine(Pens.LightGray, ci, f, cf, f);
+                y = y + 1;
+            }
+            
+            x = xi;
+            c1 = Col();
+            y = A2*fun1(x);
+
+            f1 = Fil();
+           
+            for (int k = 1; k < n; k++)
+            { 
+                x = xi + k * h;
+                y = A2 * fun1(x);
+                c = Col();
+                f = Fil();
+                g1.DrawLine(Pens.Blue, c1, f1, c, f);
+                c1 = c;
+                f1 = f;
+            }
+            
+        }
+        public int Col()
+        {
+            int Co;
+            Co = (int)(((x - xi) * cf) / (xf - xi) + ((xf - x) * ci) / (xf - xi));
+            return (Co);
+        }
+        public int Fil()
+        {
+            int Fi;
+            Fi = (int)(((y - yi) * (fi)) / (yf - yi) + ((yf - y) * ff) / (yf - yi));
+            return (Fi);
+        }
+        public double fun1(double x)
+        {
+            int valorin = listBox1.SelectedIndex;
+            string valorfu = listBox1.SelectedItem.ToString();           
+            double fx = 0;
+            double cons = 2 * Math.PI;
+            T1 = cons;
+            double aux = T1;
+            while (x > T1)
+            {
+                T1 = T1 + cons;
+            }
+            
+            if (x > (T1 - (cons)) && x < (T1 - (Math.PI)))
+            {
+                if (valorin == 0)
+                   fx = -Math.Sin(x);
+                if (valorin == 1)
+                    fx = Math.Sin(x) + 1;
+                if (valorin == 2)
+                    fx = 1;
+                if (valorin == 3)
+                 fx = Math.Sin(x);
+                if (valorin == 4)
+                    fx =  (x-(T1-cons))/((T1 - (Math.PI))-(T1 - (cons)));
+                if (valorin == 5)
+                    fx = (x - (T1 - cons)) / ((T1 - (Math.PI)) - (T1 - (cons)));
+                if (valorin == 6)
+                    fx = ((x - (T1 - cons)) / ((T1 - (Math.PI)) - (T1 - (cons))));
+                if (valorin == 7)
+                    fx =.1* Math.Tan(x);
+                if (valorin == 8)
+                    fx =  Math.Sin(x);
+                if (valorin == 9)
+                    fx = Math.Cos(x);
+                if (valorin == 10)
+                    fx = Math.Sin(x);
+                if (valorin == 11)
+                    fx = 1;
+                if (valorin == 12)
+                    fx = (x - (T1)) / ((T1 - (Math.PI)) - T1)-1;
+                if (valorin == 13)
+                    fx = (x - (T1)) / ((T1 - (Math.PI)) - T1) - 1;
+                if (valorin == 14)
+                    fx = 0;
+            }
+            if (x >(T1 - (Math.PI)) && x < T1)
+            {
+                if (valorin == 0)
+                    fx = -Math.Sin(x - Math.PI);
+                if (valorin == 1)
+                fx = Math.Sin(x) + 1;
+                if (valorin == 2 || valorin == 3)
+                fx = 0;
+                if (valorin == 4)
+                    fx = (x - (T1 )) / ((T1 - (Math.PI)) - T1);
+                if (valorin == 5)
+                    fx = -(x - (T1)) / ((T1 - (Math.PI)) - T1);
+                if (valorin == 6)
+                    fx = (-(x - (T1)) / ((T1 - (Math.PI)) - T1))+1;
+                if (valorin == 7)
+                fx = .1*Math.Tan(x);
+                if (valorin == 8)
+                    fx = Math.Sin(x);
+                if (valorin == 9)
+                    fx = Math.Cos(x);
+                if (valorin == 10)
+                    fx = Math.Sin(x - Math.PI);
+                if (valorin == 11)
+                    fx = -1;
+                if (valorin == 12)
+                    fx = (x - (T1)) / ((T1 - (Math.PI)) - T1) - 1;
+                if (valorin == 13)
+                    fx = (x - (T1)) / ((T1 - (Math.PI)) - T1) ;
+                if (valorin == 14)
+                    fx = 1;   
+            }
+
+            return (fx);
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            switch (habilitar)
+            {
+                case 1:
+                    timer1.Enabled = true;
+                    habilitar = 2;
+                    button1.Text = "Detener";                 
+                    break;
+
+                case 2:
+                    timer1.Enabled = false;
+                    habilitar = 1;
+                    button1.Text = "Iniciar";
+                    break;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            t1 = 0;
+        }
+
+        private void timer1_Tick_1(object sender, EventArgs e)
+        {
+            grafica1.Refresh();
+            Graphics g1 = grafica1.CreateGraphics();
+
+            xi = 0;
+            xf = trackBar1.Value + 0.5;
+            xi = xi + t1;
+            xf = xf + t1;
+            A2 = trackBar2.Value + 0.5;
+            Graficador(g1);
+            t1 = t1 + 0.5;
+        }     
+    }
+}
